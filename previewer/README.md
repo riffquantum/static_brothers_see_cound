847dd661882ce32fefae144880d802a185873d53
Joseph Hughes <jhughes@weblinc.com>
2019-01-01 12:05:49 -0500

    Improve previewer layout and updates shell script to watch all csd files.

--- previewer/README.md
+++ previewer/README.md
@@ -2,7 +2,7 @@
 
 This is a little preview application I wrote that will render your CSD as you save and display it with a nice waveform timeline in a web browser.
 
-From the main directory run `./previewer/preview.sh {path to file}`. That is a shell script that watches the specified file and renders it to the previewer directory whenever you save. You may need to update the permissions on that script to get it to run.
+From the main directory run `./previewer/preview.sh`. That is a shell script that watches all .csd files. When you save one it renders it to the previewer directory. You may need to update the permissions on that script to get it to run.
 
 You can then view the index.html file in a browser window to play, pause, and scrub through the waveform. If you run it through a server then auto refresh will work too. The page will refresh every time the soundfile is rendered. I recommend using pow (http://pow.cx/) along with this nifty trick to get it to serve static html: https://www.codeography.com/2014/01/17/using-pow-for-static-sites.html.
 
--- previewer/preview.sh
+++ previewer/preview.sh
@@ -7,63 +7,54 @@ csoundfolder="./"
 echo "Watching CSD files for changes"
 echo "Folder=\"$csoundfolder\""
 
-#document the file's last update time
-chksum2=`stat -f %Sm $1 | md5`;
-
-#store update time again for future comparison
-chksum1=$chksum2
-
-#The commented out code below is some in-progress work to make this script monitor all CSD files in the repo and not just the specified file. The thing holding me back at the moment is the lack of associative arrays in bash 3. I could upgrade bash or alternatively figure out how to dynamically name variables.
-
-# #create array of all csds
-# file_list=()
-# while IFS= read -d $'\0' -r file ; do
-#  file_list=("${file_list[@]}" "$file")
-# done < <(find . -name "*.csd" -print0)
-
-# echo "${file_list[@]}"
-
-
-# #document all those modified times
-# modified_times=()
-# modified_times2=()
-# index=0
-# for file in "${file_list[@]}"; do
+#create array of all csds
+file_list=()
+while IFS= read -d $'\0' -r file ; do
+ file_list=("${file_list[@]}" "$file")
+done < <(find . -name "*.csd" -print0)
 
 
 
-#     formatted_filename=`echo $file | tr -d . | tr -d /`
+#document all those modified times in an array whose indices correspond with those of the file list.
+modified_times=()
+index=0
+for file in "${file_list[@]}"; do
 
+    formatted_filename=`echo $file | tr -d . | tr -d /`
 
-#     chksum2=`stat -f %Sm $file | md5`;
+    modified_time=`stat -f %Sm $file | md5`;
 
-#     declare "modified_times_${formatted_filename}"=$chksum2
-#     declare "modified_times2_${formatted_filename}"=$chksum2
+    modified_times[$index]=$modified_time
 
-#     echo "$modified_times_${formatted_filename}"
-#     index+=1
-# done
-
-# echo "${modified_times[@]}"
-
+    index=$((index+1))
+done
 
 #loop forever
 while [[ true ]]; do
-    #check file's last update time
-    chksum2=`stat -f %Sm $1 | md5`;
 
-    #compare against old record
-    if [[ $chksum1 != $chksum2 ]] ; then
-        printf "Rendering Preview...";
-        csound $1 -W -o previewer/preview.wav
-        printf "Preview Rendered. ";
+    #loop through the file list and check for
+    index=0
+    for file in "${file_list[@]}"; do
 
-        printf "Refreshing Browser Preview";
-        touch previewer/index.html
+        formatted_filename=`echo $file | tr -d . | tr -d /`
 
-        #update old record
-        chksum1=$chksum2
-    fi
+        previous_modified_time=${modified_times[$index]}
+        modified_time=`stat -f %Sm $file | md5`;
+
+        if [[ $previous_modified_time != $modified_time ]] ; then
+            printf "Rendering Preview of ${file}...";
+            csound $file -W -o previewer/preview.wav
+            printf "Preview Rendered. ";
+
+            printf "Refreshing Browser Preview";
+            touch previewer/index.html
+
+            #update old record
+            modified_times[$index]=$modified_time
+        fi
+
+        index=$((index+1))
+    done
 
     sleep $timeinterval;
 done
