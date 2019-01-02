b81a868473ce4b1ded26f78bb4fa19951f755e47
Joseph Hughes <jhughes@weblinc.com>
2019-01-02 17:24:36 -0500

    Make previewer watch .orcs

--- previewer/README.md
+++ previewer/README.md
@@ -2,7 +2,7 @@
 
 This is a little preview application I wrote that will render your CSD as you save and display it with a nice waveform timeline in a web browser.
 
-From the main directory run `./previewer/preview.sh`. That is a shell script that watches all .csd files. When you save one it renders it to the previewer directory. You may need to update the permissions on that script to get it to run.
+From the main directory run `./previewer/preview.sh`. That is a shell script that watches all .csd and .orc files. When you save one it renders it to the previewer directory. Saving a .orc renders the most recently rendered csd or the first one in the list on the script's first render. You may need to update the permissions on that script to get it to run.
 
 You can then view the index.html file in a browser window to play, pause, and scrub through the waveform. If you run it through a server then auto refresh will work too. The page will refresh every time the soundfile is rendered. I recommend using pow (http://pow.cx/) along with this nifty trick to get it to serve static html: https://www.codeography.com/2014/01/17/using-pow-for-static-sites.html.
 
--- previewer/preview.sh
+++ previewer/preview.sh
@@ -8,23 +8,43 @@ echo "Watching CSD files for changes"
 echo "Folder=\"$csoundfolder\""
 
 #create array of all csds
-file_list=()
+csd_file_list=()
 while IFS= read -d $'\0' -r file ; do
- file_list=("${file_list[@]}" "$file")
+ csd_file_list=("${csd_file_list[@]}" "$file")
 done < <(find . -name "*.csd" -print0)
 
+#create array of all orcs
+orc_file_list=()
+while IFS= read -d $'\0' -r file ; do
+ orc_file_list=("${orc_file_list[@]}" "$file")
+done < <(find . -name "*.orc" -print0)
 
+last_rendered_file=${csd_file_list[0]}
 
-#document all those modified times in an array whose indices correspond with those of the file list.
-modified_times=()
+#document all those modified times in an array whose indices correspond with those of the csd file list.
+csd_modified_times=()
 index=0
-for file in "${file_list[@]}"; do
+for file in "${csd_file_list[@]}"; do
 
     formatted_filename=`echo $file | tr -d . | tr -d /`
 
     modified_time=`stat -f %Sm $file | md5`;
 
-    modified_times[$index]=$modified_time
+    csd_modified_times[$index]=$modified_time
+
+    index=$((index+1))
+done
+
+#document all those modified times in an array whose indices correspond with those of the orc file list.
+orc_modified_times=()
+index=0
+for file in "${orc_file_list[@]}"; do
+
+    formatted_filename=`echo $file | tr -d . | tr -d /`
+
+    modified_time=`stat -f %Sm $file | md5`;
+
+    orc_modified_times[$index]=$modified_time
 
     index=$((index+1))
 done
@@ -32,25 +52,52 @@ done
 #loop forever
 while [[ true ]]; do
 
-    #loop through the file list and check for
+    #loop through the csd file list and check for new update times
     index=0
-    for file in "${file_list[@]}"; do
+    for file in "${csd_file_list[@]}"; do
 
         formatted_filename=`echo $file | tr -d . | tr -d /`
 
-        previous_modified_time=${modified_times[$index]}
+        previous_modified_time=${csd_modified_times[$index]}
         modified_time=`stat -f %Sm $file | md5`;
 
         if [[ $previous_modified_time != $modified_time ]] ; then
             printf "Rendering Preview of ${file}...";
-            csound $file -W -o previewer/preview.wav
-            printf "Preview Rendered. ";
+            csound $file -W -o previewer/preview.wav;
+            #record the most recently rendered file
+            last_rendered_file=$file;
+            printf "Preview Rendered for ${file}. ";
 
             printf "Refreshing Browser Preview";
             touch previewer/index.html
 
             #update old record
-            modified_times[$index]=$modified_time
+            csd_modified_times[$index]=$modified_time
+        fi
+
+        index=$((index+1))
+    done
+
+    #loop through the orc file list and check for new update times
+    index=0
+    for file in "${orc_file_list[@]}"; do
+
+        formatted_filename=`echo $file | tr -d . | tr -d /`
+
+        previous_modified_time=${orc_modified_times[$index]}
+        modified_time=`stat -f %Sm $file | md5`;
+
+        if [[ $previous_modified_time != $modified_time ]] ; then
+            #render the most recently rendered CSD
+            printf "Rendering Preview of ${last_rendered_file}...";
+            csound $last_rendered_file -W -o previewer/preview.wav;
+            printf "Preview Rendered for ${last_rendered_file}. ";
+
+            printf "Refreshing Browser Preview";
+            touch previewer/index.html
+
+            #update old record
+            orc_modified_times[$index]=$modified_time
         fi
 
         index=$((index+1))
